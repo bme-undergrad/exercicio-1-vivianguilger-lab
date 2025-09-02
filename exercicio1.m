dba57ed02c4c1c328e5e8010a5c79ad048dd16dc
vivianguilger-lab <vivianguilger@gmail.com>
2025-09-02 12:24:30 -0300

    Update exercicio1.m

--- exercicio1.m
+++ exercicio1.m
@@ -1,30 +1,26 @@
-function t = exercicio1(func, func_d, x0)
+function t = exercicio1(func,func_d,x0)
 
 % nao alterar: inicio
-es = 0.01; % erro relativo
-imax = 20; % número máximo de iterações
+es = 0.01;
+imax = 20;
 % nao alterar: fim
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% digite seu codigo aqui
-t = zeros(imax,1) % alterar
-t(1) = x0
-erro = zeros(length(t),1)
+t = zeros(imax, 1);
+x(1)= x0;
+erro = zeros(length(t), 1);
 
-for ii = 1:length(t)-1
-  ii
-  if ii ~= 1
-    erro(ii) = abs((t(ii)-t(ii-1))/t(ii))
-    if erro(ii) < es
+for j = 1:length(t)-1
+  if j ~= 1
+    erro(j)=abs((x(j)-x(j-1))/x(j));
+    if erro(j)<es
       break
     endif
   endif
-  t(ii+1) = t(ii) - func(t(ii))/func_d(t(ii))
+  x(j+1) = x(j)-(func(x(j))/func_d(x(j)));
 endfor
-
-disp('O tempo é:')
-t(ii)
+t = x(j)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
